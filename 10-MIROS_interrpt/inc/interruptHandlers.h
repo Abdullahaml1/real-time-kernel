6a550b87ff4d45b57d784fa6807c11bf687a61df
Abdullahaml1 <abdullah_aml@protonmail.com>
2022-05-16 02:36:22 +0200

    adding foreground background architecture

--- 10-MIROS_interrpt/inc/interruptHandlers.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERRUPT_HANDLERS_H
-#define INTERRUPT_HANDLERS_H
-
-#include "tm4c123gh6pm.h"
-
-void SystickHandler(void);
-
-
-#endif
--- 10-MIROS_interrpt/src/main_interrupt.c
+++ 10-MIROS_interrpt/src/main_interrupt.c
@@ -1,49 +1,35 @@
-#include "tm4c123gh6pm.h"
 #include <stdint.h>
-#include <stdbool.h>
-#include "driverlib/interrupt.h" // include form tivaware 
+#include "BoardSupportPackage.h"
 
 
+void blinkGreen() {
+  bsp_LedGreenOn();
+  bsp_delay(100);
+  bsp_LedsOffAll();
+  bsp_delay(100);
+}
+
+
+
+void blinkBlue() {
+  bsp_LedBlueOn();
+  bsp_delay(100);
+  bsp_LedsOffAll();
+  bsp_delay(100);
+}
 
 
 int main(void)
 {
-
-  //-----------------------------------------------------------------------
-  // Initializing the LEDS
-  //-----------------------------------------------------------------------
-  SYSCTL_RCGCGPIO_R = 0x20U; // enable prortF clock gating
-  GPIO_PORTF_DIR_R  = 0x0EU; // direction to output
-  GPIO_PORTF_DEN_R  = 0x0EU;
-  GPIO_PORTF_DATA_R  = 0x08U; // write led
-
-  //-----------------------------------------------------------------------
-  // Initializing Systick
-  //-----------------------------------------------------------------------
-
-  // Enable interrupts to the processor.
-  IntMasterEnable();
-
-  // Enable the SysTick Interrupt.
-  /* SysTickIntEnable(); */
-
-  // Enable SysTick.
-  /* SysTickEnable(); */
-
-  // STCTRL systick status and control reegister
-  NVIC_ST_CTRL_R = 0x7;
-
-  NVIC_ST_RELOAD_R = 0xFFFFFF; //24 bit
-  NVIC_ST_CURRENT_R = 0;
-
-  /* To fire the systick maually we set the peding bit (26) in */
-  /* NVIC_INT_CTRL_R: Interrupt control and state register */
-  /* NVIC_INT_CTRL_R = NVIC_INT_CTRL_R | 0x04000000; */
+  bsp_init();
 
 
   while(1)
     {
-    }
+      blinkBlue();
+      blinkBlue();
+
+     }
   return 0;
 
 }
