711fc478e492c90ee50441dcf9ec0c229ca09f04
Abdullahaml1 <abdullah_aml@protonmail.com>
2022-05-20 23:57:36 +0200

    thanks to ALLAH adding the r4-r11 regs and increasing stack size: uart needs more stack

---
+++ 12-MIROS_sched2/inc/BoardSupportPackage.h
@@ -0,0 +1,62 @@
+#ifndef BOARD_SUPPORT_PACKAGE_H
+#define BOARD_SUPPORT_PACKAGE_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "tm4c123gh6pm.h"
+#include "driverlib/interrupt.h" // include form tivaware 
+
+
+#define CLOCK_RATE  16000000U
+#define SYSTICK_TICK_PER_SECOND  10U //10 ms
+
+void bsp_initUartPrintf();
+
+
+/*
+ * [Initializing PortF]
+ */
+void bsp_init();
+
+/**
+ * [initializing systick and pendsv]
+ */
+void bsp_os_init();
+
+
+/**
+ * [starting os]
+ */
+void bsp_os_start();
+
+
+/*
+ * [The handler for Systick Interrupt]
+ */
+void SystickHandler(void);
+
+
+
+/*
+ * [getter method for the counter]
+ * @return [returns the current systick counter]
+ */
+uint64_t bsp_getSystickCounter();
+
+
+/**
+ * [block delay using systick]
+ * @param delay [the value of the delay]
+ */
+void bsp_delay(uint32_t delay);
+
+
+void bsp_LedsOffAll();
+
+void bsp_LedGreenOn();
+void bsp_LedBlueOn();
+void bsp_LedRedOn();
+
+
+#endif
