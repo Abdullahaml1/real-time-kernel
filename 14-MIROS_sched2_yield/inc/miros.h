01cdbe4ba428380bc27663c54b714ccc44ac570a
Abdullahaml1 <abdullah_aml@protonmail.com>
2022-05-21 14:41:59 +0200

    adding yield

---
+++ 14-MIROS_sched2_yield/inc/miros.h
@@ -0,0 +1,45 @@
+#ifndef MIROS_H
+#define MIROS_H
+
+#include <stdint.h>
+
+#define THREAD_STACK_SIZE 80
+#define MAX_NUM_THREADS 100
+
+// #define DEBUG_STACK
+
+
+typedef void (*os_ThreadHandler)();
+
+typedef uint32_t os_StackElement;
+
+
+
+/*
+ * the definition of the Thread struct
+ */
+typedef struct {
+  os_StackElement *sp;
+  os_StackElement stack[THREAD_STACK_SIZE];
+} os_Thread;
+
+
+
+
+
+
+void os_init();
+
+
+void os_createThread(os_Thread *thread,
+                     os_ThreadHandler threadHandler
+                     );
+
+void os_sched();
+
+void PendsvHandler(void);
+
+void os_threadYield();
+
+
+#endif
